57e82888730b6907816fb3ef5e9e1506eb322b92
Ednalda <ednaldafakira@gmail.com>
2015-07-29 13:30:17 -0400

    add cake picture show

---
+++ scripts/cake.js
@@ -0,0 +1,10 @@
+//Description: show cake picture
+//Command: Hubot cake
+//Athor: Ednalda Fakira
+
+
+module.exports = function (){
+	return cake.respond(/cake/1, function (show){
+      show.send('http://www.picgifs.com/glitter-gifs/c/cake/picgifs-cake-8115127.gif');
+	});
+};
