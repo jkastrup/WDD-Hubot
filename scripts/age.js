cc853c33df13aa0d6fb555ed7ba55fe696f38216
Ednalda <ednaldafakira@gmail.com>
2015-07-29 11:26:59 -0400

    calculate age

---
+++ scripts/age.js
@@ -0,0 +1,12 @@
+//Description: Age calculation
+//Command: Hubot I was born in <year>
+//Author: Ednalda Fakira  ednaldafakira@yahoo.com
+
+  function calculateAge(birthYear) {
+  	var currentYear = new Date().getFullYear():
+  	return currentYear - birthYear;
+
+  }
+   document.write('Your age is: ' + calculateAge() + 'years old.');
+
+
