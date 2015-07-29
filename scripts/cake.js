5370797c5505c94bce27acf53c8f1898df961a23
Ednalda <ednaldafakira@gmail.com>
2015-07-29 14:25:19 -0400

    cake picture show added

--- scripts/cake.js
+++ scripts/cake.js
@@ -3,6 +3,7 @@
 //Athor: Ednalda Fakira
 
 
+
 module.exports = function (){
 	return cake.respond(/cake/1, function (show){
       show.send('http://www.picgifs.com/glitter-gifs/c/cake/picgifs-cake-8115127.gif');
