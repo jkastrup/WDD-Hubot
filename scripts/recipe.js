d12035e46b63d4e5f724c0f5ceae37a69c2da770
NickiSnow <snow.nicki@gmail.com>
2015-07-30 13:41:24 -0700

    added api url to function and parsed JSON to get desired data to display

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -16,14 +16,14 @@ var key = process.env.RECIPE_KEY;
 function recipe(msg){
 
     //Variable for the api url
-    var apiURL = 'http://api.campbellskitchen.com/api/BrandService.svc/search?keywords=dinner&format=json';
+    var apiURL = 'http://api.campbellskitchen.com/brandservice.svc/api/search?keywords=chicken&format=json&app_id=8ca03727&app_key=' + RECIPE_KEY;
     // API request
     request(apiURL, function (error, response, body) {
     if (!error && response.statusCode < 300){
         //parse the json
         var json = JSON.parse(response.body);
         // finds a random recipe from the api and brings back the name, url and image 
-        var recipe = "Recipe Name: "+json.data.name +"\n\n"+"Link: "+json.data.url+"\n\n"+"Description: "+json.data.image;
+        var recipe = "Recipe Name: "+json.data.recipes.name +"\n\n"+"Link: "+json.data.recipes.recipelink+"\n\n"+"Description: "+json.data.recipes.thumbimg;
                 
             msg.send(recipe);//displays the random recipe 
                 
@@ -35,7 +35,7 @@ function recipe(msg){
 
 }
 
-//Listens for the movies string and executes the function
+//Listens for the dinner string and executes the function
 module.exports = function(robot) {
   return robot.respond(/dinner$/i, function(msg) {
      recipe(msg);
