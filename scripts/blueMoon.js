3100e35fa8f25c6a70cad8218d93297cd2795dfc
Ednalda <ednaldafakira@gmail.com>
2015-07-31 19:54:46 -0400

    changed titel, website address, and picture positions to apper in the blueMoon.js file

--- scripts/blueMoon.js
+++ scripts/blueMoon.js
@@ -5,6 +5,6 @@
 module.exports = function(robot){
 	return robot.respond(/bluemoon/i, function (msg){
 	//hubot returns a blue moon title, website address and a picture.
-	   msg.send('Blue Moon', 'http://apod.nasa.gov/apod/astropix.html','http://apod.nasa.gov/apod/image/1507/STACKED-COLOUR-ISS-ODonnell.jpg');
+	   msg.send('http://apod.nasa.gov/apod/image/1507/STACKED-COLOUR-ISS-ODonnell.jpg', 'http://apod.nasa.gov/apod/astropix.html', 'Blue Moon');
 	});
 };
