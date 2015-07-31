2845238d5b4310de42203bb11a563562d0d09384
Ednalda <ednaldafakira@gmail.com>
2015-07-31 17:31:58 -0400

    fix link in lol.js file

--- scripts/lol.js
+++ scripts/lol.js
@@ -5,6 +5,6 @@
 module.exports = function (robot){
 	return robot.respond(/lol/i, function (msg){
 	  //hubot retuns an animated picture of women laughting
-	  msg.send('http://www.picgifs.com/reaction-gifs/reaction-gifs/lol-laughing/lol091 watermark.gif');
+	  msg.send('http://www.picgifs.com/reaction-gifs/reaction-gifs/lol-laughing/lol091%20watermark.gif');
 	});
 };
