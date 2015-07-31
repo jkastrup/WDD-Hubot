d0947824ac106b772f4f561a8ff0f870a083077c
Ednalda <ednaldafakira@gmail.com>
2015-07-31 19:46:49 -0400

    Add title, website address to blueMoon.js file

--- scripts/blueMoon.js
+++ scripts/blueMoon.js
@@ -4,7 +4,7 @@
 
 module.exports = function(robot){
 	return robot.respond(/bluemoon/i, function (msg){
-	//hubot returns a blue moon picture
-	   msg.send('http://apod.nasa.gov/apod/image/1507/STACKED-COLOUR-ISS-ODonnell.jpg');
+	//hubot returns a blue moon title, website address and a picture.
+	   msg.send('Blue Moon', 'http://apod.nasa.gov/apod/astropix.html','http://apod.nasa.gov/apod/image/1507/STACKED-COLOUR-ISS-ODonnell.jpg');
 	});
 };
