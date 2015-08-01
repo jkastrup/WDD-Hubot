09b16600f6e229780639af1eb4377acd51bc7cb8
Ritchie <rsoricelli@fullsail.edu>
2015-08-01 14:48:56 -0400

    Corrected spelling error in d20.js

--- scripts/d20.js
+++ scripts/d20.js
@@ -1,6 +1,6 @@
 module.exports = function (robot) {
 
-    return robot.repsond(/roll d20/i, function (res) {
+    return robot.respond(/roll d20/i, function (res) {
     
         res.send(Math.floor(Math.random() * 20 ) + 1);
         
