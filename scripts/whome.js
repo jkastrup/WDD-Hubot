513f61b5bca7af00a86b79177de759500d97835e
Ritchie <rsoricelli@fullsail.edu>
2015-08-02 13:14:25 -0400

    Erased duplicate code from merge

--- scripts/whome.js
+++ scripts/whome.js
@@ -8,7 +8,7 @@
 //None
 //
 //Commands:
-//Hubot You're really bugging me
+//Hubot stop it - Han Solo "Who, me?" gif
 //
 //Author:
 //Ritchie Soricelli
@@ -16,15 +16,9 @@
 //Contributor:
 //None
 
-module.exports = function (robot) {
-<<<<<<< HEAD
-    return robot.respond(/You're really bugging me/i, function (msg) {
-        //Han Solo "Who? Me?" gif is returned
-           msg.send('http://i.imgur.com/IKB69pa.gif');
-=======
     return robot.hear(/Stop it/i, function (res) {
         //Han Solo "Who? Me?" gif is returned
         res.send('http://i.imgur.com/IKB69pa.gif');
->>>>>>> testing
+
     });
 };
