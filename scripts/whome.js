57c877250b17ef7d97d0abd5a2da493bb871b0e8
Ritchie <rsoricelli@fullsail.edu>
2015-08-02 13:18:30 -0400

    replaced module.exports

--- scripts/whome.js
+++ scripts/whome.js
@@ -16,6 +16,7 @@
 //Contributor:
 //None
 
+module.exports = function (robot) {
     return robot.hear(/Stop it/i, function (res) {
         //Han Solo "Who? Me?" gif is returned
         res.send('http://i.imgur.com/IKB69pa.gif');
