fa7e588c974f27066f9b9d84296c360f261407fe
stevedolan <stevedolan@fullsail.edu>
2015-08-04 13:49:43 -0700

    Added quotation marks to the quote

--- scripts/coffee.js
+++ scripts/coffee.js
@@ -20,7 +20,7 @@ module.exports = function(robot){
     // Hubot returns a message and an image from Jerry Seinfeld
     return robot.respond(/coffee/i,function(msg){
         //Sends a quote
-        msg.send('We want to do a lot of stuff; we’re not in great shape. We didn’t get a good night’s sleep. We’re a little depressed. Coffee solves all these problems in one delightful little cup. - Jerry Seinfeld');
+        msg.send('"We want to do a lot of stuff; we’re not in great shape. We didn’t get a good night’s sleep. We’re a little depressed. Coffee solves all these problems in one delightful little cup." - Jerry Seinfeld');
         //Sends an images of Jerry
         msg.send('http://1.bp.blogspot.com/-AGdMdfvtPbQ/Vae-YAhtnVI/AAAAAAAAFRg/OPKwgGBVzTA/s1600/coffee-and-breakfast1.png');
     });
