d97f3e878b879db825b693ce2af641e1a507b009
joannamok <joannamok90@gmail.com>
2015-09-27 20:03:02 -0500

    testing rock js

--- scripts/rock.js
+++ scripts/rock.js
@@ -1,2 +1,14 @@
-//this is my js for hubbot
 
+module.exports = function (robot) {
+    return robot.respond(/rock me/i, function (msg){
+        
+        msg.send('https://en.wikipedia.org/wiki/Malachite');
+    });
+};
+
+module.exports = function (robot) {
+    return robot.respond(/bling me/i, function (msg){
+        
+        msg.send('https://www.facebook.com/hajcz.bizuteria.artystyczna/photos/pb.1430745793812984.-2207520000.1443402043./1716708861883341/?type=3&theater');
+    });
+};
