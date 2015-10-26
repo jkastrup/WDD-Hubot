521edb4b997e2e08bde7d8f6f36fddd26371a81b
julieveal <julieclucas@att.net>
2015-10-25 20:44:39 -0400

    -a

--- scripts/dog.js
+++ scripts/dog.js
@@ -12,6 +12,9 @@
 // julieclucas@att.net
 
 
-module.exports = (robot) ->
-    robot.hear /have you seen my dog/i, (res) ->        
-        res.send "https://www.petfinder.com/pet-adoption/dog-adoption/"
+module.exports = function (robot) {
+    return robot.respond(/have you seen my dog/i, function (msg){
+        
+        msg.send('https://www.petfinder.com/pet-adoption/dog-adoption/');
+    });
+};
